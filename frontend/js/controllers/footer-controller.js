f50cdeca35c48168660ce6817df70f26fbfbedbb
sailirawnang <avinashghare572@gmail.com>
2017-11-29 16:21:52 +0530

    enquiry

--- frontend/js/controllers/footer-controller.js
+++ frontend/js/controllers/footer-controller.js
@@ -5,19 +5,44 @@ myApp.controller('footerCtrl', function ($scope, TemplateService, NavigationServ
     });
     $scope.showMsg=false
     console.log("inside footerctrl")
-$scope.submitEmail=function(data){
+// $scope.submitEmail=function(data){
 
-     NavigationService.saveMail(data, function (data1) {
-         console.log("go",data1);
-          if(data1.data.value==true){
-              console.log("inside if")
-               $scope.showMsg=false
-               $scope.data={};
-         }else{
-                   console.log("inside else")
-             $scope.showMsg=true
-         }
-      });
-}
+//      NavigationService.saveMail(data, function (data1) {
+//          console.log("go",data1);
+//           if(data1.data.value==true){
+//               console.log("inside if")
+//                $scope.showMsg=false
+//                $scope.data={};
+//          }else{
+//                    console.log("inside else")
+//              $scope.showMsg=true
+//          }
+//       });
+// }
+  $scope.subscribeSubmit = function(subscribeData) {
+            console.log("sadsadasdsads");
+            console.log("subscribeData", subscribeData);
+           NavigationService.saveMail(subscribeData, function (data) {
+                console.log("data", data.value);
+                if (data.data.data) {
+                    console.log('hhhhhhhhh');
+                    $scope.subscribeComplete = true;
+                    $timeout(function() {
+                        $scope.subscribeComplete = false;
+                        $scope.subscribeData = {};
+                    }, 2000);
+                } else {
+                    console.log('eeeeeeeeeee');
+                    console.log('inside elseeee');
+                    $scope.alreadySubscribed = true;
+                    $timeout(function() {
+                        $scope.alreadySubscribed = false;
+                        $scope.subscribeData = {};
+                    }, 2000);
+                }
+
+
+            })
+        }
 
 });
